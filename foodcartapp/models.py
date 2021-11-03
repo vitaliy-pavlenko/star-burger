9ed6807c72f640bd3bcbeb5b6cb39e924a56e919
Vitaliy Pavlenko <vpavlenko@iponweb.net>
2021-11-03 18:33:00 +0300

    Add registered, called and delivered datetimes for order

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils import timezone
 from phonenumber_field.modelfields import PhoneNumberField
 
 
@@ -141,6 +142,9 @@ class Order(models.Model):
     address = models.CharField(max_length=200, verbose_name='Адрес')
     status = models.IntegerField(choices=STATUS_CHOICES, default=NEW, verbose_name='Статус')
     comment = models.TextField(max_length=300, blank=True, verbose_name='Комментарий к заказу')
+    registered_at = models.DateTimeField(default=timezone.now, verbose_name='Время регистрации заказа')
+    called_at = models.DateTimeField(null=True, blank=True, verbose_name='Время звонка менеджера')
+    delivered_at = models.DateTimeField(null=True, blank=True, verbose_name='Время доставки')
 
     def __str__(self):
         return f'{self.firstname} {self.lastname} {self.address}'

6cfe44a077563d68b7c7c08c0b4c17a671f86d49
Vitaliy Pavlenko <vpavlenko@iponweb.net>
2021-11-03 18:26:01 +0300

    Add comment for order

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -140,6 +140,7 @@ class Order(models.Model):
     phonenumber = PhoneNumberField(max_length=15, verbose_name='Телефон')
     address = models.CharField(max_length=200, verbose_name='Адрес')
     status = models.IntegerField(choices=STATUS_CHOICES, default=NEW, verbose_name='Статус')
+    comment = models.TextField(max_length=300, blank=True, verbose_name='Комментарий к заказу')
 
     def __str__(self):
         return f'{self.firstname} {self.lastname} {self.address}'
